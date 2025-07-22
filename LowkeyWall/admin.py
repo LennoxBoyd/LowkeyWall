50c6e4f06768b57bb1a0c305542c226c571d9f59
LennoxBoyd <trentarnoldalexandar@gmail.com>
2025-07-22 19:09:07 +0000

    Fix: Add upvote_count_display to ConfessionAdmin

--- LowkeyWall/admin.py
+++ LowkeyWall/admin.py
@@ -3,9 +3,14 @@ from .models import Confession, Comment, Quote, SupportPlan, Reply, Upvote
 
 @admin.register(Confession)
 class ConfessionAdmin(admin.ModelAdmin):
-    list_display = ('topic', 'created_at', 'upvote_count', 'is_featured')
+    list_display = ('topic', 'created_at', 'upvote_count_display', 'is_featured')
     list_filter = ('topic', 'is_featured', 'created_at')
     search_fields = ('topic', 'message')
+
+    def upvote_count_display(self, obj):
+        return obj.upvote_count
+
+    upvote_count_display.short_description = 'Upvotes'
 
 admin.site.register(Comment)
 admin.site.register(Quote)
@@ -15,6 +20,7 @@ admin.site.register(Upvote)
 @admin.register(SupportPlan)
 class SupportPlanAdmin(admin.ModelAdmin):
     list_display = ['name', 'price_usd']
+
 
 
 
