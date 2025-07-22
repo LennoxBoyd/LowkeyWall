c33b614a192716434f5f8690374bf1b01acb7b0a
Lennox Boyd <trentarnoldalexandar@gmail.com>
2025-07-22 05:48:10 -0700

    Update views.py

--- LowkeyWall/views.py
+++ LowkeyWall/views.py
@@ -13,7 +13,6 @@ import qrcode
 from io import BytesIO
 from django.http import HttpResponse
 from django.urls import reverse
-from django.db import models  # ✅ Make sure you have this at the top
 
 
 
